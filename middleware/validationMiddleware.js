3a50d0f73fd32848807cf7b1aa0bfd8ff140073f
Darina-Shvydiuk <darinakabakova@gmail.com>
2022-11-13 18:20:23 +0200

    hw done

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -1,31 +1,42 @@
 import Joi from "joi";
 
 export const addContactsValidation = (req, res, next) => {
-  const schema = Joi.object({
-    name: Joi.string().min(3).max(20).required(),
-    email: Joi.string()
-      .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-      .required(),
-    phone: Joi.string().min(12).max(20).required(),
-  });
-  const validationResult = schema.validate(req.body);
-  if (validationResult.error) {
-    return res.status(400).json({ status: validationResult.error.details });
-  }
-  next();
+    const schema = Joi.object({
+        name: Joi.string().min(3).max(20).required(),
+        email: Joi.string()
+            .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+            .required(),
+        phone: Joi.string().min(12).max(20).required(),
+    });
+    const validationResult = schema.validate(req.body);
+    if (validationResult.error) {
+        return res.status(400).json({ status: validationResult.error.details });
+    }
+    next();
 };
 
 export const updateContactsValidation = (req, res, next) => {
-  const schema = Joi.object({
-    name: Joi.string().min(3).max(20).required(),
-    email: Joi.string()
-      .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-      .required(),
-    phone: Joi.string().min(12).max(20).required(),
-  });
-  const validationResult = schema.validate(req.body);
-  if (validationResult.error) {
-    return res.status(400).json({ status: validationResult.error.details });
-  }
-  next();
+    const schema = Joi.object({
+        name: Joi.string().min(3).max(20).required(),
+        email: Joi.string()
+            .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+            .required(),
+        phone: Joi.string().min(12).max(20).required(),
+    });
+    const validationResult = schema.validate(req.body);
+    if (validationResult.error) {
+        return res.status(400).json({ status: validationResult.error.details });
+    }
+    next();
+};
+
+export const updateContactsValidationFavorite = (req, res, next) => {
+    const schema = Joi.object({
+        favorite: Joi.boolean().required(),
+    });
+    const validationResult = schema.validate(req.body);
+    if (validationResult.error) {
+        return res.status(400).json({ status: validationResult.error.details });
+    }
+    next();
 };
